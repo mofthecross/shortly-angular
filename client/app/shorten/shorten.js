7a3804fc0f5e32dcbd11c2d0d1cbca8093ae0b73
mofthecross <mofthecross@gmail.com>
2016-07-08 19:02:51 -0700

    Add ShortenController and Shorten route

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,17 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function(url) {
+    Links.addOne(url)
+      .then(function(response) {
+        console.log('addLink', response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+  };
+
 });
